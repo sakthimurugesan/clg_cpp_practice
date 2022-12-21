234f8f54b7590aad9420ccf10ab494497cdd772f
SAKTHI M <msakthi150@gmail.com>
2022-12-21 09:45:40 +0530

    commit

---
+++ operators.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x=25;
+    float y=10,z=x/y; //implicit type convertion
+    /*
+    float higher data type 
+    int lower data type
+    */
+    cout<<z;
+}
