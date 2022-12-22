5709d606f8baa6b1c13a6c14c5aaed79c9cc0cf7
SAKTHI M <msakthi150@gmail.com>
2022-12-22 09:11:22 +0530

    controlstatement

---
+++ controlstatement.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+    /*
+    
+    __________________________________________________________________
+
+    simple if  -----------> one choice
+
+    if else -----------> two choice
+    
+    nested if else -----------> one if else statement can be writen on another if else multiple choice
+
+    else if ladder -----------> many choice more than 3
+
+    switch ----------->
+    
+    
+    __________________________________________________________________
+    
+    */
+}
