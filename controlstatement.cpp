ba1b6e04cd1516152b4f6fb9c8a5af442b8e8116
SAKTHI M <msakthi150@gmail.com>
2022-12-22 09:23:28 +0530

    commit

--- controlstatement.cpp
+++ controlstatement.cpp
@@ -1,8 +1,35 @@
 
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 int main(){
-    /*
+
+   /*
+        write a program to read a choice from the user
+        if choice is 1 read radius of circle and find area of circle 
+        if choice is 2 read l,b of rectangle find its area
+
+   */
+
+
+    int choi;
+    cin>>choi;
+    switch (choi)
+    {
+    case 1:
+        float r;
+        cin>>r;
+        cout<<3.14*r*r;
+        break;
+    case 2:
+        int l,b;
+        cin>>l>>b;
+        cout<<l*b;
+    default:
+    cout<<"Wrong input sorry !!."
+;        break;
+    }
+
+        /*
     
     __________________________________________________________________
 
@@ -15,9 +42,10 @@ int main(){
     else if ladder -----------> many choice more than 3
 
     switch -----------> working with integer or char
-    
-    
+
     __________________________________________________________________
     
     */
+
+
 }
